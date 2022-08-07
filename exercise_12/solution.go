d54db432792297d2bd5bc28b311b4c7c9d2204a3
GLOBANT\pranav.manole <pranav.manole@globant.com>
2022-08-07 16:29:18 +0530

    exercise 11 attempted and 12 solved

---
+++ exercise_12/solution.go
@@ -0,0 +1,51 @@
+package exercise_12
+
+func IntToRoman(num int) string {
+	roman := ""
+	counter := 1
+	for num != 0 {
+		if counter == 1 {
+			roman = getRomanForRange("I", "V", "X", num) + roman
+		}
+		if counter == 2 {
+			roman = getRomanForRange("X", "L", "C", num) + roman
+		}
+		if counter == 3 {
+			roman = getRomanForRange("C", "D", "M", num) + roman
+		}
+		if counter == 4 {
+			n := num % 10
+			if n < 4 && n != 0 {
+				for n > 0 {
+					roman = "M" + roman
+					n--
+				}
+			}
+		}
+		num = num / 10
+		counter++
+	}
+	return roman
+}
+
+func getRomanForRange(fL, mL, lL string, num int) string {
+	roman := ""
+	n := num % 10
+	if n < 4 && n != 0 {
+		for n > 0 {
+			roman += fL
+			n--
+		}
+	} else if n == 4 {
+		roman = fL + mL
+	} else if n >= 5 && n < 9 {
+		for n > 5 {
+			roman += fL
+			n--
+		}
+		roman = mL + roman
+	} else if n == 9 {
+		roman = fL + lL
+	}
+	return roman
+}
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	// exercise_11 "leetcode/exercise_11"
+	exercise_12 "leetcode/exercise_12"
+)
+
+func main() {
+	// ans := exercise_11.MaxArea(exercise_11.Sl)
+	// fmt.Printf("Ans: %t", ans == 49)
+	fmt.Println(exercise_12.IntToRoman(17))
+	fmt.Println(exercise_12.IntToRoman(20))
+	fmt.Println(exercise_12.IntToRoman(33))
+	fmt.Println(exercise_12.IntToRoman(45))
+	fmt.Println(exercise_12.IntToRoman(58))
+	fmt.Println(exercise_12.IntToRoman(69))
+	fmt.Println(exercise_12.IntToRoman(99))
+	fmt.Println(exercise_12.IntToRoman(77))
+	fmt.Println(exercise_12.IntToRoman(1994))
+	// fmt.Println(exercise_12.IntToRoman(10))
+	// fmt.Println(exercise_12.IntToRoman(20))
+	// fmt.Println(exercise_12.IntToRoman(30))
+}
